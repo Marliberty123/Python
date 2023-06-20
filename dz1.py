e5a6fd6ae39c476aaedcb31ed72113a39ad02e93
Mariia I <marliberty123@gmail.com>
2023-06-20 16:39:46 +0300

    cod

---
+++ dz1.py
@@ -0,0 +1,8 @@
+#Задача 2: Найдите сумму цифр трехзначного числа.
+
+a = int(input("Vvedite trehznachnoe chislo: "))
+c = a % 10 #poclednee chislo
+a = a //10 #ubiraem poslednee chislo
+b = a % 10 #chislo v centre
+a = a // 10 #ubiraem chislo v centre
+print(f' Summa ravna {a+b+c}')
